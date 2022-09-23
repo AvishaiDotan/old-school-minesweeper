b52feda07aa9c5cad4ee46b43c9ab51baed88b81
AvishaiDotan <108017307+AvishaiDotan@users.noreply.github.com>
2022-09-23 16:37:04 +0300

    Update sounds.js

--- js/sounds.js
+++ js/sounds.js
@@ -2,15 +2,15 @@
 'use strict'
 
 const GAME_SOUNDS = {
-    BACKGROUND: new Audio(`sounds/background-sound.wav`),
-    MAIN_ERROR: new Audio(`sounds//error-sound-effect.mp3`),
-    WIN: new Audio(`sounds/winning-sound-effect.mp3`),
-    CLICK: new Audio(`sounds/click.wav`),
-    LOSE_SOUND: new Audio(`sounds/lose_minesweeper.wav`),
-    FLAG: new Audio(`sounds/insert-flag.wav`),
-    HINT: new Audio(`sounds/hint.wav`),
-    RESET: new Audio(`sounds/reset.wav`),
-    EXPAND: new Audio(`sounds/expand-open.wav`),
+    BACKGROUND: new Audio(`./sounds/background-sound.wav`),
+    MAIN_ERROR: new Audio(`./sounds//error-sound-effect.mp3`),
+    WIN: new Audio(`./sounds/winning-sound-effect.mp3`),
+    CLICK: new Audio(`./sounds/click.wav`),
+    LOSE_SOUND: new Audio(`./sounds/lose_minesweeper.wav`),
+    FLAG: new Audio(`./sounds/insert-flag.wav`),
+    HINT: new Audio(`./sounds/hint.wav`),
+    RESET: new Audio(`./sounds/reset.wav`),
+    EXPAND: new Audio(`./sounds/expand-open.wav`),
 }
 
 function playBackgroundSound() {
